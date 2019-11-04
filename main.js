23a2fbb33f6a7fbc97ef396e59588c8a9fcb47f2
Nerevin <kazak.redalerd@gmail.com>
2019-11-04 23:02:38 +0400

    feat: add keyboard animation

--- main.js
+++ main.js
@@ -2,22 +2,30 @@ const russianKeyboard = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\',
 'Caps Lock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter',
 'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', 'Shift',
-'Ctrl', 'Win', 'Alt', 'Space', 'Alt', 'Win', 'OS', 'Ctrl'];
+'Ctrl', 'Win', 'Alt', 'Space', 'AltGr', 'Win', 'OS', 'Ctrl'];
 
 const englishKeyboard = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\',
 'Caps Lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter',
 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'Shift',
-'Ctrl', 'Win', 'Alt', 'Space', 'Alt', 'Win', 'OS', 'Ctrl'];
+'Ctrl', 'Win', 'Alt', 'Space', 'AltGr', 'Win', 'OS', 'Ctrl'];
 
-function init() {
+const keyCode = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace',
+'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash',
+'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter',
+'ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ShiftRight',
+'ControlLeft', 'OSLeft', 'AltLeft', 'Space', 'AltRight', 'OSRight', 'ContextMenu', 'ControlRight'];
+
+window.onload = function init() {
     let textArea = document.createElement('textarea');
     textArea.className = 'input';
     textArea.setAttribute('rows', '6');
     textArea.setAttribute('cols', '60');
+    textArea.setAttribute('autofocus', '');
+    textArea.addEventListener('blur', textArea.focus);
     document.body.appendChild(textArea);
 
-    let wrapper =document.createElement('div');
+    let wrapper = document.createElement('div');
     wrapper.className = 'keyboard';
     document.body.appendChild(wrapper);
 
@@ -29,6 +37,7 @@ function init() {
 
     for (let i = 0; i < russianKeyboard.length; i++) {
         let key = document.createElement('div');
+        key.setAttribute('data-keyCode', keyCode[i]);
         key.innerHTML = russianKeyboard[i];
         if (russianKeyboard[i] == 'Backspace') key.className = 'keyboard__backspace'
         else if (russianKeyboard[i] == 'Tab') key.className = 'keyboard__tab'
@@ -36,13 +45,76 @@ function init() {
         else if (russianKeyboard[i] == 'Enter') key.className = 'keyboard__enter'
         else if (russianKeyboard[i] == 'Shift') key.className = 'keyboard__shift'
         else if (russianKeyboard[i] == 'Space') key.className = 'keyboard__space'
-        else key.className = 'key'
+        key.classList.add('key');
         if (i < 14) document.querySelector("body > div > div:nth-child(1)").appendChild(key);
         else if (i < 28) document.querySelector("body > div > div:nth-child(2)").appendChild(key);
         else if (i < 41) document.querySelector("body > div > div:nth-child(3)").appendChild(key);
         else if (i < 53) document.querySelector("body > div > div:nth-child(4)").appendChild(key);
         else document.querySelector("body > div > div:nth-child(5)").appendChild(key);
     }
+
+    let language = document.createElement('div');
+    language.className = 'language';
+    language.innerHTML = 'Change language: ';
+    document.body.appendChild(language);
+    language = document.createElement('select');
+    document.querySelector("body > div.language").appendChild(language);
+    language = document.createElement('option');
+    language.innerHTML = 'Rus';
+    document.querySelector("body > div.language > select").appendChild(language);
+    language = document.createElement('option');
+    language.innerHTML = 'Eng';
+    document.querySelector("body > div.language > select").appendChild(language);
+    changeLang();
 }
 
-init()
+document.addEventListener('keydown', function(event){
+    let key = document.getElementsByClassName('key');
+    if (event.code == 'ShiftLeft' || event.code == 'ShiftRight')  {
+        for(let i = 0; i < key.length; i++) {
+            key[i].classList.add('key__uppercase');
+        }
+    }
+    if (event.code == 'CapsLock') {
+        for(let i = 0; i < key.length; i++) {
+            key[i].classList.toggle('key__uppercase');
+        }
+    }
+    for(let i = 0; i < key.length; i++) {
+        if (key[i].getAttribute('data-keyCode') == event.code) key[i].classList.add('key__keydown');
+    }
+    let textArea = document.getElementsByClassName('input');
+    textArea[0].value += event.key;
+});
+
+document.addEventListener('keyup', function(event){
+    event.preventDefault();
+    let key = document.getElementsByClassName('key');
+    if (event.code == 'ShiftLeft' || event.code == 'ShiftRight')  {
+        for(let i = 0; i < key.length; i++) {
+            key[i].classList.remove('key__uppercase');
+        }
+    }
+    for(let i = 0; i < key.length; i++) {
+        if (key[i].getAttribute('data-keyCode') == event.code) key[i].classList.remove('key__keydown');
+    }
+});
+
+function changeLang() {
+    let select = document.querySelector("body > div.language > select");
+    select.addEventListener('change', function() {
+        if (this.value == 'Rus') {
+            for (let i = 0; i < russianKeyboard.length; i++) {
+                let key = document.getElementsByClassName('key');
+                key[i].innerHTML = russianKeyboard[i];
+            }
+        }
+        else if (this.value == 'Eng') {
+            for (let i = 0; i < englishKeyboard.length; i++) {
+                let key = document.getElementsByClassName('key');
+                key[i].innerHTML = englishKeyboard[i];
+            }
+        }
+        localStorage.setItem('lang', this.value);
+    })
+}
